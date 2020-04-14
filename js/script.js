e04eff5de26680f22fdd297912b05a899f4ee6c4
James Estrada <Jamestrada@users.noreply.github.com>
2020-04-14 08:40:27 -0600

    Added project starter files

---
+++ js/script.js
@@ -0,0 +1,29 @@
+/******************************************
+Treehouse FSJS Techdegree:
+Project 1 - A Random Quote Generator
+******************************************/
+
+/*** 
+ * `quotes` array 
+***/
+
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
