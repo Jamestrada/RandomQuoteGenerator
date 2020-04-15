578fb8014ef1c9023bf8ac505a2861e8f9246fee
James Estrada <Jamestrada@users.noreply.github.com>
2020-04-15 13:16:46 -0600

    - Added Array of Quotes Objects

--- js/script.js
+++ js/script.js
@@ -6,19 +6,45 @@ Project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
-
+const quotes = [
+    {
+        quote: '"Failure doesn\'t matter: you only have to be right once."',
+        source: 'Drew Houston',
+        citation: 'https://news.mit.edu/2013/commencement-address-houston-0607',
+        year: 2013
+    },
+    {
+        quote: '"Luck is what happens when preparation meets opportunity"',
+        source: 'Seneca'
+    },
+    {
+        quote: '"Just because someone stumbles and loses their way, it doesn\'t mean they\'re lost forever"',
+        source: 'Professor X (Patrick Stewart)',
+        citation: 'X-Men: Days of Future Past',
+        year: 2014
+    },
+    {
+        quote: '"You, me, or nobody is gonna hit as hard as life. But it ain’t about how hard you hit. It’s about how hard you can get hit and keep moving forward."',
+        source: 'Rocky Balboa (Sylvester Stallone)',
+        citation: 'Rocky Balboa',
+        year: 2006
+    },
+    {
+        quote: '“Ask and it will be given to you; seek and you will find; knock and the door will be opened to you.',
+        source: 'Jesus Christ',
+        citation: 'Matthew 7:7 NIV'
+    }
+];
+// console.log(quotes);
 
 /***
  * `getRandomQuote` function
 ***/
 
 
-
 /***
  * `printQuote` function
 ***/
-
 
 
 /***
