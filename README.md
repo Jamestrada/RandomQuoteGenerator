a4a51a98a5b4aad90ad80bb850c82f1c7df22454
James Estrada <Jamestrada@users.noreply.github.com>
2020-04-16 13:02:08 -0600

    Added tags property. Added random background color generator. Added timing function to auto-refresh quotes.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Random Quote Generator
- Display random famous quotes each time a button is clicked.
+ Display random famous quotes each time a button is clicked or a period of time has passed.
--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 /******************************************
 Treehouse FSJS Techdegree:
 Project 1 - A Random Quote Generator
+Developed by: James Estrada
 
 The random quote generator displays a 
 random quote each time a button is clicked 
@@ -11,37 +12,49 @@ or a period of time has passed.
 const quotes = [
     {
         quote: "Failure doesn't matter: you only have to be right once.",
-        source: 'Drew Houston',
-        citation: 'Commencement at MIT',
+        source: "Drew Houston",
+        citation: "Commencement at MIT",
         year: 2013
     },
     {
         quote: "Luck is what happens when preparation meets opportunity",
-        source: 'Seneca'
+        source: "Seneca"
     },
     {
         quote: "Just because someone stumbles and loses their way, it doesn't mean they're lost forever",
-        source: 'Professor X (Patrick Stewart)',
-        citation: 'X-Men: Days of Future Past',
-        year: 2014
+        source: "Professor X (Patrick Stewart)",
+        citation: "X-Men: Days of Future Past",
+        year: 2014,
+        tags: ["[film]"]
     },
     {
-        quote: "You, me, or nobody is gonna hit as hard as life. But it ain’t about how hard you hit. It’s about how hard you can get hit and keep moving forward.",
-        source: 'Rocky Balboa (Sylvester Stallone)',
-        citation: 'Rocky Balboa',
-        year: 2006
+        quote: "It does not matter how slowly you go as long as you do not stop.",
+        source: "Confucius",
+        tags: ["[motivational]", "[wisdom]", "[wordstoliveby]"]
     },
     {
         quote: "Ask and it will be given to you; seek and you will find; knock and the door will be opened to you.",
-        source: 'Jesus Christ',
-        citation: 'Matthew 7:7 NIV'
+        source: "Jesus Christ",
+        citation: "Matthew 7:7 NIV",
+        tags: ["[bible]", "[religion]"]
+    },
+    {
+        quote: "You, me, or nobody is gonna hit as hard as life. But it ain’t about how hard you hit. It’s about how hard you can get hit and keep moving forward.",
+        source: "Rocky Balboa (Sylvester Stallone)",
+        citation: "Rocky Balboa",
+        year: 2006,
+        tags: ["[film]"]
+    },
+    {
+        quote: "I have not failed. I've just found 10,000 ways that won't work.",
+        source: "Thomas Edison"
     }
 ];
 
 /**
  * Create a random number to return a random quote object from the quotes array.
  * 
- * @returns {object} A quote object that contains 'quote' and 'source' as required properties; 'citation' and 'year' as optionals.
+ * @returns {object} A quote object that contains 'quote' and 'source' as required properties; 'citation', 'year', and 'tag' as optionals.
  */
 const getRandomQuote = () => {
     const randomNumber = Math.floor(Math.random() * quotes.length);
@@ -49,11 +62,21 @@ const getRandomQuote = () => {
 };
 
 /**
+ * Create a random number from an 8-bit color possible representation.
+ * 
+ * @returns {number} A random number from 0 to 255 to use it as the intensity for each rgb value.
+ */
+const getRandomColor = () => Math.floor(Math.random() * 256);
+
+/**
  * Get a random quote object and display it in the browser.
  * 
  * @returns {undefined} Display quote in the browser through a concatenated html string.
  */
 function printQuote() {
+    // Select the body tag and change the background color to a random one.
+    document.querySelector('body').style.backgroundColor = `rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()})`;
+
     const quote = getRandomQuote();
     let html = `<p class="quote">${quote.quote}</p><p class="source">${quote.source}`;
     if (quote.citation) {
@@ -62,9 +85,15 @@ function printQuote() {
     if (quote.year) {
         html += `<span class="year">${quote.year}</span>`;
     }
+    if (quote.tags) {
+        html += `<span style="font-size: 0.9em; font-weight: bold">&ensp;${quote.tags.join(' ')}</span>`; // Inline CSS for new tags property; join list of tag(s) with space.
+    }
     html += `</p>`;
     document.getElementById('quote-box').innerHTML = html;
 }
+
+// Pass the function printQuote as a reference into setInterval's handler so it can be called every 17 seconds.
+setInterval(printQuote, 17000);
 
 /***
  * click event listener for the print quote button
